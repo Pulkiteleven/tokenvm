4ed506995826eeea271294947f448a60cd4c9232
Pulkiteleven <92856883+Pulkiteleven@users.noreply.github.com>
2024-05-22 02:05:48 +0530

    First Commit

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Pulkit Chain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "PULKIT"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "PUL"
 )
 
 var ID ids.ID
